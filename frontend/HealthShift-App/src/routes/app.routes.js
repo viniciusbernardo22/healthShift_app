1af959452b0576466f01eea34b3672e95d5a86ca
Vinicius <vini383@gmail.com>
2024-04-18 17:07:49 -0300

    feat: add custom drawer

--- frontend/HealthShift-App/src/routes/app.routes.js
+++ frontend/HealthShift-App/src/routes/app.routes.js
@@ -3,12 +3,15 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Home from '../screens/Home/home';
 import Detail from '../screens/Detail/detail';
+import CustomDrawer from '../custom/drawer/CustomDrawer';
 const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
   return (
     <>
-      <AppDrawer.Navigator screenOptions={{ drawerType: 'front' }}>
+      <AppDrawer.Navigator
+        drawerContent={CustomDrawer}
+      >
         <AppDrawer.Screen name='Home' component={Home} />
         <AppDrawer.Screen
           name='Detalhes'
